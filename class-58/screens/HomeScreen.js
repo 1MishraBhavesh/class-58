5cb739ccdc82dd66ba68b8a3ec060816c51ea642
1MishraBhavesh <74255631+1MishraBhavesh@users.noreply.github.com>
2021-04-23 20:45:35 +0530

    Update HomeScreen.js

--- class-58/screens/HomeScreen.js
+++ class-58/screens/HomeScreen.js
@@ -1,12 +1,38 @@
 import * as React from 'react';
 import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
 import AppHeader from '../components/AppHeader'
+import db from '../config'
 
  export default class HomeScreen extends React.Component{
+
+   constructor(){ 
+     super();
+     this.state={
+       redStatus:true,
+       blueStatus:true,        
+       greenStatus:true,
+       yellowStatus:true
+     };
+   }
  goToBuzzerScreen=(buzzerColor) => {
+   var teamRef=db.ref('teams/' + buzzerColor);
+   teamRef.update({  
+     enabled:false
+   });
    this.props.navigation.navigate('BuzzerScreen',{color:buzzerColor})
  }
- 
+ componentDidMount(){
+    var teamRef=db.ref('teams/');
+  teamRef.on("value",(data)=>{        
+                       var  teamDetails= data.val();
+                         this.setState({
+                           redStatus:teamDetails.red.enabled,
+                             blueStatus:teamDetails.blue.enabled,
+                               greenStatus:teamDetails.green.enabled,
+                                 yellowStatus:teamDetails.yellow.enabled
+                         });
+                }); 
+ }
  
   render(){
     return(
@@ -14,22 +40,35 @@ import AppHeader from '../components/AppHeader'
        <AppHeader/>
        
        <TouchableOpacity
+       disabled={!this.state.redStatus}
+         
+              
+          
    style={[styles.button,
     { backgroundColor: 'red' }]}
+    
       onPress={() => { this.goToBuzzerScreen('red'); }}>
         <Text style={styles.buttonText}>Team 1</Text>
          </TouchableOpacity>
+
          <TouchableOpacity
+          disabled={!this.state.blueStatus}  
    style={[styles.button,
     { backgroundColor: 'blue' }]}
       onPress={() => { this.goToBuzzerScreen('blue'); }}>
         <Text style={styles.buttonText}>Team2</Text>
-         </TouchableOpacity><TouchableOpacity
+         </TouchableOpacity>
+
+         <TouchableOpacity
+         disabled={!this.state.yellowStatus}
    style={[styles.button,
     { backgroundColor: 'yellow' }]}
       onPress={() => { this.goToBuzzerScreen('yellow'); }}>
         <Text style={styles.buttonText}>Team3</Text>
-         </TouchableOpacity><TouchableOpacity
+         </TouchableOpacity> 
+
+         <TouchableOpacity
+         disabled={!this.state.greenStatus}
    style={[styles.button,
     { backgroundColor: 'green' }]}
       onPress={() => { this.goToBuzzerScreen('green'); }}>
@@ -47,7 +86,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignSelf: 'center',
     borderWidth: 2,
-    borderRadius: 20,
+    borderRadius: 20, 
     width: 200,
     heigth: 50,
     marginTop: 90,
